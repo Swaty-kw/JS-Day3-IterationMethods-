a19cb63f8227cfd381d5617d390da871a3a86a0d
Swaty-kw <dashtilimited@gmail.com>
2024-08-07 14:22:29 +0300

    Done

---
+++ iterationMethods.js
@@ -0,0 +1,61 @@
+const numbers = [10, 13, 20, 25, 38, 35, 40];
+
+/*****************************************************************
+Part 1: Filtering
+
+Write code to perform the following tasks using the filter method:
+Q1) Create a new array containing numbers that are greater than or equal to 25.
+Q2) Create a new array containing numbers that are divisible by 5.
+******************************************************************/
+
+/*let numbersBiggerThan25 = numbers.filter((number) => {
+  if (number >= 15) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+const numbersdevivedby5 = numbers.filter((number) => {
+  if (number % 5 == 0) {
+    return true;
+  } else {
+    false;
+  }
+});
+console.log(numbersBiggerThan25);
+console.log(numbersdevivedby5);
+
+let numSquare = numbers.map((number) => {
+  return n * n;
+});
+let numberx2 = numbers.map((number) => {
+  return n ** 2;
+});
+console.log(numSquare);
+console.log(numberx2); */
+
+let sum1 = numbers.filter((number) => {
+  if (number >= 20) {
+    return true;
+  } else {
+     false;
+  }
+});
+let squaredNumbers = numbers.map((number) => {
+  return number * number;
+});
+//console.log(sum1);
+//console.log(squaredNumbers);
+
+let devi5 = numbers.filter((number) => {
+  if (number % 5 == 0);{
+    return true  
+  } else {false;}
+    
+  
+  
+  
+    
+  
+}); console.log(devi5)
